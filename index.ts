66f6fbdfc0e1251fd5702e1298f92928b5d63d09
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-04-11 14:42:57 +0700

    remove comment

--- index.ts
+++ index.ts
@@ -4,11 +4,10 @@ import * as cors from "cors"
 import * as fs from "fs"
 import { Server, Socket } from "socket.io"
 import * as bodyParser from "body-parser"
+import { ActiveSession, ActiveSessions } from './services/sessionService'; 
+import { v4 as uuid } from "uuid"
 
 import routes from "./routes"
-
-import FileRouter from "./routes/file.routes"
-
 
 const app = express();
 
@@ -16,7 +15,6 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }))
 
-// app.use("/api/files", FileRouter)
 app.use('/api/v1', routes)
 
 app.get('/', (req, res) => {
@@ -28,15 +26,11 @@ const io = new Server(server)
 
 const port = process.env.port || 8999
 
-// const wss = new WebSocket.Server({ server })
+const sessionTracker = new ActiveSessions()
 
 // Socket.io
 io.on('connection', (socket: Socket) => {
   console.log("a user connected")
-
-  // socket.on('syncReq', (id) => {
-  //   console.log(`syncReq ${id}`)
-  // })
   socket.on('syncReq', data => {
     const filename = data.filename
     const username = data.username
@@ -61,110 +55,10 @@ io.on('connection', (socket: Socket) => {
     }
   })
 
-  socket.on('chat message', (msg) => {
-    console.log(msg)
-  })
-
   socket.on('disconnect', () => {
     console.log('user disconnected')
   })
 })
-
-// WS
-// interface ExtWebSocket extends WebSocket {
-//   isAlive: Boolean;
-// }
-
-// wss.on('connection', (ws: WebSocket) => {
-//   const extWs = ws as ExtWebSocket;
-//   extWs.isAlive = true;
-
-//   ws.on('pong', () => {
-//     extWs.isAlive = true;
-//   })
-
-//   ws.on('message', (data: string) => {
-
-//     let file = {} as FileModel
-
-//     try {
-//       file = JSON.parse(data) as FileModel
-//     } catch (e) {
-//       ws.send("failed to parse data")
-//       ws.close()
-//     }
-
-//     let fileId = file.id
-//     let path = `./files/${fileId}.txt`
-//     console.log(`action: ${file.action}`)
-//     console.log(`id: ${file.id}`)
-
-//     switch (file.action) {
-//       case "read":
-//         try {
-//           fs.readFile(path, (err, data) => {
-//             if (err) throw err
-      
-//             return ws.send(data)
-//           })
-//         } catch (err) {
-//           ws.send(`file not found, closing connection`)
-//           ws.close()
-//         }
-//         break;
-//       case "update":
-//         fs.writeFile(`${path}.txt`, file.content, () => {
-//           ws.send("failed to update file")
-//         })
-//         break;
-//       default:
-//         ws.send("bad data")
-//         break;
-//     }
-
-    // if (file.action === 'get') {
-    //   try {
-    //     fs.readFile(path, (err, data) => {
-    //       if (err) throw err
-    
-    //       return ws.send(data)
-    //     })
-    //   } catch (err) {
-    //     ws.send(`file not found, closing connection`)
-    //     ws.close()
-    //   }
-    // }
-  // })
-
-  // ws.on('update', (payload: string) => {
-  //   let file = JSON.parse(payload) as FileModel
-  //   let fileId = file.id
-  //   let path = `./files/${fileId}.txt`
-    
-  //   try {
-  //     if (fs.existsSync(path)) {
-  //       next()
-  //     } else {
-  //       throw new Error(`${path} not found`)
-  //     }
-  //   } catch (err) {
-  //     console.log(err)
-  //     res.status(404).send("file not found")
-  //   }
-  // })
-
-  // ws.send(`Hi there, I am a websocket server`);
-// })
-
-// setInterval(() => {
-//   wss.clients.forEach((ws: WebSocket) => {
-//     const extWs = ws as ExtWebSocket;
-
-//     if (!extWs.isAlive) return ws.terminate();
-//     extWs.isAlive = false;
-//     ws.ping(null, undefined)
-//   })
-// }, 5000)
 
 server.listen(port, () => {
   console.log(`Server started on port ${port} :)`);
